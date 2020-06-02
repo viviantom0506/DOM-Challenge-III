586e6f35fce3e1a3378d85e1be6c89ab87e522c3
Sahal Asghar <sahaldos1@gmail.com>
2020-06-01 20:11:04 -0400

    added remove row function

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ function addRow(){
   const TableElements = document.getElementById("Table")
   let NewRow = document.createElement("tr")
   TableElements.appendChild(NewRow);
+  NewRow.classList.add("row");
   let column=1;
   for(let i=0; i<column;i++){
       let cell = document.createElement("td");
       NewRow.appendChild(cell);
       cell.textContent = "O";
   }
+}
+
+function removeRow() {
+  let row = document.getElementsByClassName("row");
+  let tr = Array.from(row);
+  tr[tr.length - 1].parentNode.removeChild(tr[tr.length - 1]);
 }
