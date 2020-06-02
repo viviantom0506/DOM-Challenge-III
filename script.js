da4aedad65511c139005ad098a1b0ada3989ef93
viviantom0506 <icemarina123@gmail.com>
2020-06-01 22:34:06 -0400

    finished the rest of the functions

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function addRows(){
   table.appendChild(tableRow);
   for (let i = 0; i < columns; i++) {
     let tableCell = document.createElement("td");
+    colorChange(tableCell);
     tableRow.appendChild(tableCell);
   }
 }
@@ -17,10 +18,11 @@ function addColumns(){
   tr = [...rows];
   for (let i = 0; i < tr.length; i++) {
     let tableCell = document.createElement("td");
+    colorChange(tableCell);
     tr[i].appendChild(tableCell);
   }
 }
-//allows the user to remove 
+//allows the user to remove the row with the remove row button
 function removeRow(){
   let rows = document.getElementsByClassName("row");
   tr = [...rows];
@@ -32,7 +34,6 @@ function removeRow(){
     columns = 1;
   }
 }
-
 //allows the user to remove a column when the remove column button is pressed.
 function removeColumn(){
   if (columns == 1) {
@@ -46,3 +47,59 @@ function removeColumn(){
   }
 }
 let selectedColor = "blue";
+const selectColor = (color) => {
+  selectedColor = color;
+};
+//changes color of cells
+let isColored = false;
+function colorChange(tableCell) {
+  tableCell.classList.add("notColored");
+  tableCell.addEventListener("click", changeColor);
+  tableCell.addEventListener("mousedown", (e) => {
+    isColored = true;
+  });
+  tableCell.addEventListener("mousemove", (e) => {
+    if (isColored) {
+      tableCell.style.backgroundColor = selectedColor;
+    }
+  });
+  tableCell.addEventListener("mouseup", (e) => {
+    if (isColored) {
+      isColored = false;
+    }
+  });
+}
+function changeColor() {
+  this.style.backgroundColor = selectedColor;
+  this.classList.remove("notColored");
+}
+//fills uncolored cells
+function fillUncolored() {
+  let tableCells = document.getElementsByTagName("td");
+  let collectionOfCells = [...tableCells];
+  let uncoloredtableCells = collectionOfCells.filter((tableCell) => {
+    return tableCell.classList.contains("notColored");
+  });
+  uncoloredtableCells.forEach((tableCell) => {
+    tableCell.style.backgroundColor = selectedColor;
+    tableCell.classList.remove("notColored");
+  });
+}
+//fill up all cells with selected color
+function fillAll() {
+  let tableCells = document.getElementsByTagName("td");
+  let collectionOfCells = [...tableCells];
+  collectionOfCells.forEach((tableCell) => {
+    tableCell.style.backgroundColor = selectedColor;
+    tableCell.classList.remove("notColored");
+  });
+}
+//cleared up all cells of selected color
+function clearAll() {
+    let tableCells = document.getElementsByTagName("td");
+    let collectionOfCells = [...tableCells];
+    collectionOfCells.forEach((tableCell) => {
+      tableCell.style.backgroundColor = "beige";
+      tableCell.classList.add("notColored");
+    });
+  }
